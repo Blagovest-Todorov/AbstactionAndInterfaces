8d80f5f10d9d8af9f542805e2846328aaddb77a0
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-07-08 13:08:32 +0300

    Add Project Car

---
+++ StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Gray");
+            Car tesla = new Tesla(2, "Model3", "Red");           
+            
+            Console.WriteLine(seat);
+            Console.WriteLine(tesla);
+        }
+    }
+}
---
+++ Tesla.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : Car, IElectricCar
+    {
+        public int Battery { get; set ; }
+        
+        public Tesla(int battery, string model, string color)
+            : base(model, color)
+        {
+            Battery = battery;  
+        }        
+
+        public override string ToString()
+        {
+            return $"{Color} Tesla {Model} with {Battery} Baterries";
+        }
+    }
+}
